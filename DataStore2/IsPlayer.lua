6b79e038f0557e1c8fecb8b372f8fb88f57c3fe1
howmanysmall <lhamil29@bruinmail.slcc.edu>
2020-03-15 13:57:54 -0600

    d

--- DataStore2/IsPlayer.lua
+++ DataStore2/IsPlayer.lua
@@ -1,21 +1,8 @@
 -- This function is monkey patched to return MockDataStoreService during tests
 local IsPlayer = {}
 
-local typeof = function(object)
-	local realTypeOf = typeof(object)
-	if realTypeOf == "table" then
-		if object.__type then
-			return object.__type
-		else
-			return realTypeOf
-		end
-	else
-		return realTypeOf
-	end
-end
-
 function IsPlayer.Check(object)
-	return typeof(object) == "Instance" and object.ClassName == "Player"
+	return typeof(object) == "Instance" and object:IsA("Player")
 end
 
 return IsPlayer
